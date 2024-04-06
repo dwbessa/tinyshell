0bb3bff2d5bc1a38fe4148e432c47a70330a8b96
dwbessa <danielbessa01@gmail.com>
2024-04-06 17:58:48 -0300

    fix: norminette

--- src/expander/expand_prompt.c
+++ src/expander/expand_prompt.c
@@ -6,57 +6,71 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 16:26:12 by dbessa            #+#    #+#             */
-/*   Updated: 2024/04/06 17:30:00 by dbessa           ###   ########.fr       */
+/*   Updated: 2024/04/06 17:58:26 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char	*handle_dollar_question(char *arg, unsigned int g_exit_status)
+{
+	free(arg);
+	return (ft_itoa(g_exit_status));
+}
+
+char	*handle_dollar(char *arg, t_list *env)
+{
+	t_list	*aux;
+	int		len;
+
+	aux = env;
+	while (aux)
+	{
+		len = ft_strlen(arg + 1);
+		if (!ft_strncmp(arg + 1, aux->content, len))
+		{
+			free(arg);
+			return (ft_strdup(ft_strchr(aux->content, '=') + 1));
+		}
+		aux = aux->next;
+	}
+	return (arg);
+}
+
+char	*handle_tilde(char *arg, t_list *env)
+{
+	t_list	*aux;
+	char	*temp;
+
+	aux = env;
+	while (aux)
+	{
+		if (!ft_strncmp(aux->content, "HOME=", 5))
+		{
+			temp = ft_strjoin(aux->content + 5, arg + 1);
+			free(arg);
+			arg = temp;
+			break ;
+		}
+		aux = aux->next;
+	}
+	return (arg);
+}
+
 char	**expand_prompt(char **arg, t_list **env)
 {
-	t_list				*aux;
-	char				*temp;
 	int					i;
-	int					len;
 	extern unsigned int	g_exit_status;
 
 	i = 0;
 	while (arg[i])
 	{
-		aux = *env;
 		if (!ft_strncmp(arg[i], "$?", 3))
-		{
-			free(arg[i]);
-			arg[i] = ft_itoa(g_exit_status);
-		}
+			arg[i] = handle_dollar_question(arg[i], g_exit_status);
 		else if (!ft_strncmp(arg[i], "$", 1))
-		{
-			while (aux)
-			{
-				len = ft_strlen(arg[i] + 1);
-				if (!ft_strncmp(arg[i] + 1, aux->content, len))
-				{
-					free(arg[i]);
-					arg[i] = ft_strdup(ft_strchr(aux->content, '=') + 1);
-					break ;
-				}
-				aux = aux->next;
-			}
-		}
+			arg[i] = handle_dollar(arg[i], *env);
 		else if (!ft_strncmp(arg[i], "~", 1))
-		{
-			while (aux)
-			{
-				if (!ft_strncmp(aux->content, "HOME=", 5))
-				{
-					temp = ft_strjoin(aux->content + 5, arg[i] + 1);
-					free(arg[i]);
-					arg[i] = temp;
-					break ;
-				}
-				aux = aux->next;
-			}
-		}
+			arg[i] = handle_tilde(arg[i], *env);
 		i++;
 	}
 	return (arg);
