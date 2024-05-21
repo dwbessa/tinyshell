0023a363947598071d77b01e1eedca9bb9c10642
dbessa <danielbessa01@gmail.com>
2024-05-21 08:55:43 -0300

    fix: norminette

--- src/utils/prompt_utils.c
+++ src/utils/prompt_utils.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 17:17:35 by dbessa            #+#    #+#             */
-/*   Updated: 2024/05/20 21:43:10 by dbessa           ###   ########.fr       */
+/*   Updated: 2024/05/21 08:55:15 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static char	*get_user(t_list *env, char *output)
 		}
 		env = env->next;
 		if (!env)
-			output =  ft_strjoin(output, "marvin");
+			output = ft_strjoin(output, "marvin");
 	}
 	return (output);
 }
